d9a97c1bc10a9447ab468eb5952fc4d520ec5cc5
George Corney <haxiomic@gmail.com>
2024-07-06 19:36:41 +0100

    Improve useStateAndRef.ts (instant set ref)

--- react/useStateAndRef.ts
+++ react/useStateAndRef.ts
@@ -1,11 +1,27 @@
-import { useRef, useState } from "react"
+import { Dispatch, SetStateAction, useRef, useState } from "react"
 
 /**
  * Returns [state, setState, ref] where ref is a ref to the current state.
+ * ref is updated immediately when setState is called.
  */
 export function useStateAndRef<T>(initialValue: T) {
   const [state, setState] = useState(initialValue)
   const ref = useRef(state)
-  ref.current = state
-  return [state, setState, ref] as const
+  const setStateAndRef: Dispatch<SetStateAction<T>> = (value: T | ((prev: T) => void)) => {
+    if (typeof value === "function") {
+      setState((prev) => {
+        const next = (value as (prev: T) => T)(prev);
+        ref.current = next
+        return next
+      })
+    } else {
+      ref.current = value
+      setState(value)
+    }
+  }
+  return [
+    state,
+    setStateAndRef,
+    ref
+  ] as const
 }
