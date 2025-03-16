782b3237aad29cf1ffa0f57f59f5ff0b35a3381a
haxiomic <haxiomic@gmail.com>
2025-03-16 22:37:27 +0000

    Add RawShaderMaterial with typed uniforms

--- materials/RawShaderMaterial.ts
+++ materials/RawShaderMaterial.ts
@@ -2,11 +2,11 @@
  * Improves typing of ShaderMaterial by adding a generic type parameter for uniforms.
  */
 
-import { IUniform, ShaderMaterial as ShaderMaterialBase, ShaderMaterialParameters } from "three";
+import { IUniform, RawShaderMaterial as RawShaderMaterialBase, ShaderMaterialParameters } from "three";
 
 type UniformsRecord = Record<string, IUniform<any>>;
 
-export class ShaderMaterial<T extends UniformsRecord = any> extends ShaderMaterialBase {
+export class RawShaderMaterial<T extends UniformsRecord = any> extends RawShaderMaterialBase {
   declare uniforms: T;
 
   constructor(parameters: Omit<ShaderMaterialParameters, 'uniforms'> & { uniforms: T }) {
