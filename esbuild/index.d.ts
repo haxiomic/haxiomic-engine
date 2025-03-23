7416d0f9ab267464b6a187cf0e248c620ba7c4ad
haxiomic <haxiomic@gmail.com>
2025-03-23 18:58:38 +0000

    Try expose types

---
+++ esbuild/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./compile-time.d.ts" />
+/// <reference path="./glsl-minify-plugin.d.ts" />
+/// <reference path="./inline-worker-plugin.d.ts" />
---
+++ types.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="./esbuild/index.d.ts" />
